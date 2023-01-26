cb74c1bef954feabeef23c34c5dc53baeea5ec96
WilliamDMP <wdmartinez89@gmail.com>
2023-01-25 21:16:30 -0500

    recorderis

---
+++ Basic/inicio.js
@@ -0,0 +1,9 @@
+let angelaGysseth = "novia"
+const angysseth = 'Super cansona'
+
+const cansona = {
+    nombre: "guayabo",
+    edad: 60,
+    esCansona: true
+}
+console.log(angelaGysseth, angysseth, cansona.esCansona, cansona.edad)
