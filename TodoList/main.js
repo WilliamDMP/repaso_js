2d36ca44de87985615274eaaa728ab96f3ded202
WilliamDMP <wdmartinez89@gmail.com>
2023-03-14 21:28:58 -0500

    todo list

--- TodoList/main.js
+++ TodoList/main.js
@@ -1,11 +1,22 @@
+const arregloTodo = [] 
+
 const main = () => {
+
     const formulario = document.getElementById('todo-list')
     formulario.onsubmit = (e) => {
+
         e.preventDefault()
-        const campo = document.getElementById('campoTexto')
+        const campo = document.getElementById('campoTexto')   
         campoTexto = campo.value
-        campoTexto.value = ''
-        console.log(campoTexto)
+        campo.value = ''
+        console.log(campoTexto)  
+        arregloTodo.push(campoTexto)
+        const elementoLista = document.getElementById('elemento-lista')
+        elementoLista.innerHTML = ''
         
+        for (let index = 0; index < arregloTodo.length; index++) {
+            elementoLista.innerHTML = elementoLista.innerHTML + '<li>' + arregloTodo[index] + '</li>' 
+        }
+
     } 
 }
